0899ad499526b1bcb1cc4c12fe851bf8e3ea872e
seize <cz2123zc@gmail.com>
2024-07-06 02:51:32 +0900

    디폴트 컨트롤러 생성

---
+++ src/main/java/com/example/service02/controller/DefaultController.java
@@ -0,0 +1,24 @@
+package com.example.service02.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/service-02")
+@Slf4j
+public class DefaultController {
+
+    private final Environment environment;
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("service02 is being called");
+        return "Hello from service01 running at port: " + environment.getProperty("local.server.port");
+    }
+
+}
